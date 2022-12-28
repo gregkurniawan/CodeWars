94a32bba0a0267e45ae771265b19b99ac585c2df
gregkurniawan <gregkurniawan1@gmail.com>
2022-12-28 10:54:50 -0800

    Multiply by 8 of even number, by 9 otherwise.

---
+++ 8kyu/simple-multiplication.js
@@ -0,0 +1,6 @@
+// DESCRIPTION:
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+
+//My solution
+const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9
