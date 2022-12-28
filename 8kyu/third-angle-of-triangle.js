25b8c67473ecf0175df445fdf4a3cdceaa546b9c
gregkurniawan <gregkurniawan1@gmail.com>
2022-12-28 11:58:13 -0800

    Given 2 interior angles, return the 3rd angle.

---
+++ 8kyu/third-angle-of-triangle.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+
+//My solution
+const otherAngle = (a, b) => 180 - a - b
