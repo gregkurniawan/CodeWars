b76dba6c47e78d9831fca3490eb72ba292a30418
gregkurniawan <gregkurniawan1@gmail.com>
2022-12-28 12:58:58 -0800

    Return the sum of the sum of numbers.

---
+++ 8kyu/square-sum.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+//My solution
+const squareSum = numbers => numbers.map(num => Math.pow(num, 2)).reduce((a, b) => a + b, 0)
+
+//Alternate solution
+function squareSum(numbers) {
+    return numbers.map(num => Math.pow(num, 2)).reduce((a, b) => a + b, 0)
+}
