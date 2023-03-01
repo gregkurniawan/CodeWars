1780a4d7a44f358ba8e41b9a8840930a99e73b6c
gregkurniawan <gregkurniawan1@gmail.com>
2023-03-01 13:43:06 -0800

    Return its position in the alphabet.

---
+++ 8kyu/find-the-position.js
@@ -0,0 +1,9 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+
+//My solution
+const position = letter => `Position of alphabet: ${letter.charCodeAt() - 96}`
