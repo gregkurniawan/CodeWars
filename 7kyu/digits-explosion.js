78b7afa92b185ea0a4587ea4f2c00a92f4091185
gregkurniawan <gregkurniawan1@gmail.com>
2023-05-03 22:30:00 -0700

    Return a string where each digit is repeated a number of times equal to its value.

---
+++ 7kyu/digits-explosion.js
@@ -0,0 +1,15 @@
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+// Examples
+// explode("312")
+// should return :
+
+// "333122"
+// explode("102269")
+// should return :
+
+// "12222666666999999999"
+
+
+//My solution
+const explode = s => s.replace(/\d/g, d => d.repeat(d))
